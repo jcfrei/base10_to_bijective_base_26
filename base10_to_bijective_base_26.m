de25d4c34ee3c858cac5a076f395b35ca901b6ed
Jonathan Frei <jcf@torusinvestments.ch>
2015-11-24 19:25:20 +0100

    spelling

--- base10_to_bijective_base_26.m
+++ base10_to_bijective_base_26.m
@@ -1,6 +1,6 @@
 function [out] = base10_to_bijective_base_26(m)
-    % converts a base 10 number to a bijective base-26 number
-    % bijective base-26 is use for example in Excel columns
+    % converts a base-10 number to a bijective base-26 number [A ... Z AA AB ...]
+    % bijective base-26 is used for example in Excel columns
     %
     % input: int
     % output: char
